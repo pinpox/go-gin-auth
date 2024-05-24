ddedac1923a7f30c7b7b7fe304ef9fbb7e9be477
Pablo Ovelleiro Corral <mail@pablo.tools>
2024-05-25 00:25:44 +0200

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,55 @@
+{
+  description = "Minimal viable demo CRUD application with authentication";
+
+  # Nixpkgs / NixOS version to use.
+  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+
+      # to work with older version of flakes
+      lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
+
+      # Generate a user-friendly version number.
+      version = builtins.substring 0 8 lastModifiedDate;
+
+      # System types to support.
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+
+      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      # Nixpkgs instantiated for supported system types.
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+
+    in
+    {
+
+      # Provide some binary packages for selected system types.
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
+          go-gin-auth = pkgs.buildGoModule {
+            pname = "go-gin-auth";
+            inherit version;
+            src = ./.;
+            vendorHash = "sha256-4793xq58Dqvlw9rsgd8yWyyzUmRL//t78Jc47YlTlVg=";
+          };
+        });
+
+      # Add dependencies that are only needed for development
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [ go gopls gotools go-tools gcc fd entr];
+          };
+        });
+
+      defaultPackage = forAllSystems (system: self.packages.${system}.go-gin-auth);
+    };
+}
